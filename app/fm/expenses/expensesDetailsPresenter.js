98817c5baf29a2e84b281e85e501c0162aa7fe5d
HaoHo <haho@codix.eu>
2017-10-20 11:35:51 +0700

    Initialize the project.

---
+++ app/fm/expenses/expensesDetailsPresenter.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ExpensesDetailsPresenter = /** @class */ (function () {
+    function ExpensesDetailsPresenter() {
+    }
+    return ExpensesDetailsPresenter;
+}());
+exports.ExpensesDetailsPresenter = ExpensesDetailsPresenter;
+//# sourceMappingURL=expensesDetailsPresenter.js.map
---
+++ app/fm/expenses/paymentMethod.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PaymentMethod = /** @class */ (function () {
+    function PaymentMethod() {
+    }
+    return PaymentMethod;
+}());
+exports.PaymentMethod = PaymentMethod;
+//# sourceMappingURL=paymentMethod.js.map
